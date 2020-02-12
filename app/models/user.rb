8281795bba3f76b32c7e8da8876122d200dc8e2a
pradhan150 <ppradhan@gammastack.com>
2020-02-12 19:18:30 +0530

    created migration of user

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  validates :name, :email, :password, :date_of_birth, :mobile_number, :gender, presence: true
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+  validates :password, length: { in: 6..20 }
+  validates :email, :mobile_number, uniqueness: true
+  validate :age_must_be_greater_than_13
+
+  def age_must_be_greater_than_13
+    if Date.today.year - date_of_birth.year < 13
+      errors.add(:date_of_birth, "age must be greater than or equal to 13")
+    end
+  end
+end
